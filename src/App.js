dcd1e5339488a842d79bf69f6c6d535ffb12d45b
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-11-04 15:50:32 -0800

    added icons and imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { routes, route, link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons';
+import Calculator from './Calculator';
+import ExchangeTable from './ExchangeTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,2 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons';
